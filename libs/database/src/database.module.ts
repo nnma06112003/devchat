8345082d5b8975b8eb4ce40f3e286f8786027a3f
nguyenthaibinhduong <natteam1998@gmail.com>
2025-08-30 10:23:19 +0700

    fix

--- libs/database/src/database.module.ts
+++ libs/database/src/database.module.ts
@@ -10,14 +10,14 @@ import * as Entities from '@myorg/entities';
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
-        host: config.get('DB_HOST') || 'localhost',
+        host: 'localhost',
         port: parseInt(config.get('POSTGRES_PORT') || '5432', 10),
         username: config.get('POSTGRES_USER') || 'postgres',
         password: config.get('POSTGRES_PASSWORD') || 'password',
         database: config.get('POSTGRES_DB') || 'dev_chat',
         entities: Object.values(Entities),
         migrations: ['dist/libs/database/migrations/*.js'],
-        synchronize: process.env.NODE_ENV === 'development'
+        synchronize: true,
       })
     })
   ]
