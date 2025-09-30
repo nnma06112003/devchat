3b6955cbc24930c8d1c1d7994bde15c535db3c95
nguyenthaibinhduong <natteam1998@gmail.com>
2025-09-30 16:09:20 +0700

    'ok'

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -25,7 +25,7 @@ import path from 'path';
         secret:
           configService.get('ACCESS_SECRET_KEY') ||
           'nguyenthaibinhduongdevchatappaccess',
-        signOptions: { expiresIn: '60s' },
+        signOptions: { expiresIn: "10s" },
       }),
     }),
 
