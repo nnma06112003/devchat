27dd462589a9e93f0fdde3d6f1e503a4ae2c59a1
nguyenthaibinhduong <natteam1998@gmail.com>
2025-10-06 16:42:14 +0700

    'ok'

--- apps/gateway/src/socket.service.ts
+++ apps/gateway/src/socket.service.ts
@@ -267,27 +267,27 @@ export class ChatSocketService {
           status: 'sent',
         });
       }
-      // const result = await this.gw.exec('notification', 'send_notification', {
-      //   ...res,
-      //   type:'message',
-      // });
+      const result = await this.gw.exec('notification', 'send_notification', {
+        ...res,
+        type:'message',
+      });
 
-      // if (result?.data) {
-      //   for (const notify of result?.data.notifications) {
-      //   const statusStr = await this.redis.hget('user_status', notify?.userId);
-      //   if (!statusStr) continue;
-      //   const status = JSON.parse(statusStr);
-      //   if (status.online && status.socketId && this.server) {
-      //     this.server.to(status.socketId).emit('receiveNotification', {
-      //       ...notify ,
-      //       fakeID: tempId,
-      //     });
-      //     console.log(
-      //       `📢 Sent channel to user ${notify?.userId} at socket ${status.socketId}`,
-      //     );
-      //   }
-      // }
-      // }
+      if (result?.data) {
+        for (const notify of result?.data.notifications) {
+        const statusStr = await this.redis.hget('user_status', notify?.userId);
+        if (!statusStr) continue;
+        const status = JSON.parse(statusStr);
+        if (status.online && status.socketId && this.server) {
+          this.server.to(status.socketId).emit('receiveNotification', {
+            ...notify ,
+            fakeID: tempId,
+          });
+          console.log(
+            `📢 Sent channel to user ${notify?.userId} at socket ${status.socketId}`,
+          );
+        }
+      }
+      }
       await this.incrementUnread(
         String(message.channelId),
         String(message.user.id),
